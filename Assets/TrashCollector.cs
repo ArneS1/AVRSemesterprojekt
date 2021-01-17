1ad5fa023fb4710fce7507e32dcf4e0e2c5b5dae
ArneS1 <arnoold12@gmx.de>
2021-01-17 20:41:41 +0100

    Collecting Trash

---
+++ Assets/TrashCollector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCollector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //When Trash enters the Trashbin
+    private void OnTriggerEnter(Collider other){
+
+        if(other.gameObject.CompareTag("Trash")){
+
+            other.gameObject.SetActive(false);
+            Debug.Log("Trash Collected.");
+            //TODO: Score 
+        }
+    }
+}
