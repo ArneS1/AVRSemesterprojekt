42fa800aef1d6fbd6d84af510fd12c96b27beb02
ArneS1 <arnoold12@gmx.de>
2021-01-19 12:12:08 +0100

    Trapped Fish + Cuttable Elements

---
+++ Assets/Cuttable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuttable : MonoBehaviour
+{
+    public GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collided with" + other.tag);
+        
+        if(other.gameObject.CompareTag("Plier")){
+
+            Debug.Log("I was cut by a Plier.");
+            self.SetActive(false);
+
+        }
+    }
+}
