f7f2c208070a9afc357c1e9dd0a7c9b77dde6e33
ArneS1 <arnoold12@gmx.de>
2021-01-20 20:56:36 +0100

    fishing net Task + Fishing net look rework

---
+++ Assets/Scrips/Cuttable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuttable : MonoBehaviour
+{
+    private GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Plier")){
+
+            Debug.Log("I was cut by a Plier.");
+            self.SetActive(false);
+
+        }
+    }
+}
